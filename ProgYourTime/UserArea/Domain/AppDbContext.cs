e7e32824d3238dcbf5dbdb458f43a4aa2d351871
EvgeniyGnetnev <chaklittle@yandex.ru>
2022-10-26 16:29:00 +0300

    Изменил тематику сайта с отслеживания веса на отслеживания прогресса обучения в разных сферах.

--- ProgYourTime/UserArea/Domain/AppDbContext.cs
+++ ProgYourTime/UserArea/Domain/AppDbContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProgYourTime.UserArea.Domain
 {
-    public class AppDbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
