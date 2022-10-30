9d6184b51f591117126a965d7b2f90805ed52197
EvgeniyGnetnev <chaklittle@yandex.ru>
2022-10-30 23:11:12 +0300

    - Добавил переход на страницу регистрации по кнопкам. - Создал модель страницы входа. - Создал страницу для входа. - Добавил переход на страницу входа по кнопкам.

--- ProgYourTime/Domain/ApplicationContext.cs
+++ ProgYourTime/Domain/ApplicationContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using ProgYourTime.UserArea.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +11,5 @@ namespace ProgYourTime.UserArea.Domain
     public class ApplicationContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=localhost;Database=ProgYourTime; Persist Security Info=false; User ID=sa;Password=sa;MultipleActiveResultSets=True;Trusted_Connection=False;");
-        }*/
-
     }
 }
